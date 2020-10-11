7cc9f46b3edc1df07e9fc165a708d675553e8a2b
Daniel Sebrek <dani.sebrek@gmail.com>
2020-10-11 14:44:30 +0200

    portfolio store in memory

--- VaRgyak5/VaRgyak5/Form1.cs
+++ VaRgyak5/VaRgyak5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaRgyak5.Entities;
 
 namespace VaRgyak5
 {
@@ -14,11 +15,21 @@ namespace VaRgyak5
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
+            CreatePortfolio();
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
