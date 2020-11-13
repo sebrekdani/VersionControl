41f00d51718b7dac076d087b5a6cb0d19901bd13
Daniel Sebrek <dani.sebrek@gmail.com>
2020-11-13 13:11:06 +0100

    8feladat

---
+++ Gyak08_BQZ42F/Gyak08_BQZ42F/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak08_BQZ42F.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
