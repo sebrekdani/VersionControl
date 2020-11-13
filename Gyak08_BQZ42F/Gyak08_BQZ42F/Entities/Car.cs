934de5b452054cf1e3fb0ae555914b7688b40092
Daniel Sebrek <dani.sebrek@gmail.com>
2020-11-13 13:06:25 +0100

    7feladat

---
+++ Gyak08_BQZ42F/Gyak08_BQZ42F/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Gyak08_BQZ42F.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak08_BQZ42F.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
